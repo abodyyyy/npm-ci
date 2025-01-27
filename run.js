1c18868d782c3a3be4f213fb82c7b367270a8be4
abodyyyy <131920581+abodyyyy@users.noreply.github.com>
2025-01-27 15:56:00 +0100

    Add files via upload

---
+++ run.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getPackageInfo(packageName) {
+  const response = await axios.get(`https://registry.npmjs.org/${packageName}`);
+  return response.data;
+}
+
+let successCount = 0;
+
+async function downloadTarball(tarballUrl, idx = 0) {
+  await sleep(idx * 10);
+  await axios(tarballUrl);
+
+  successCount++;
+
+  console.log(`[${idx + 1}] Successfully downloaded!`);
+}
+
+async function spamPackageDownload(packageName, count) {
+  try {
+    // Get package info from npm registry
+    const packageInfo = await getPackageInfo(packageName);
+    const latestVersion = packageInfo["dist-tags"].latest;
+    const tarballUrl = packageInfo.versions[latestVersion].dist.tarball;
+
+    const responses = [];
+
+    for (let i = 0; i < count; i++) {
+      responses.push(downloadTarball(tarballUrl, i));
+    }
+
+    await Promise.all(responses);
+
+    console.log(successCount, "/", count, "downloads completed!");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+const downloadcCount = parseInt(process.argv[2]) || 10;
+
+spamPackageDownload("react-pre-hooks", downloadcCount);
